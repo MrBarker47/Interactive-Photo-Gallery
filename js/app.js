897c3104407f39d6cba016287a51d5cef08eb8ca
MrBarker47 <jebarker7@gmail.com>
2022-06-30 13:30:37 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+baguetteBox.run('.wrapper');
+
+
+
---
+++ js/searchFilter.js
@@ -0,0 +1,36 @@
+/**
+ * searchFilter.js
+ * 
+ * Functionality:
+ *  As the user types into the search input, only photos that match the caption text appear.
+ *  Case insensitivity has been added, searches will ignore letter case. 
+ * 
+ * Installation: 
+ *  Add searchFilter.js into your js folder
+ *  Add this line into your index.html <script src="js/searchFilter.js"></script>
+ *   
+ * Utilization:
+ *  Ensure that your search input is setup like this in your index.html:
+ *   <input type="search" name="search" id="search" placeholder="Search">
+ *  Ensure that your image captions are added to your anchor elements via the data-caption attribute:
+ *   <a href="photos/my-image.jpg" data-caption="A picture of me coding JavaScript on my computer">
+ *  Add this into your app.js file:
+ *   const search = new Filter('search', 'data-caption');
+ */
+
+const itemList = [
+    "Hay Bales",
+    "Lake",
+    "Canyon",
+    "Iceberg",
+    "Desert",
+    "Fall",
+    "Plantation",
+    "Dunes",
+    "Countryside Lane",
+    "Sunset",
+    "Cave",
+    "Bluebells",
+];
+
+const itemContainer = document.getElementById("item-list");
