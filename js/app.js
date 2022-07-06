5ad86ebc3c56cf51e89ece193b4d761b41cccc56
MrBarker47 <jebarker7@gmail.com>
2022-07-05 20:45:54 -0400

    update files

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 baguetteBox.run('.wrapper');
 
+let searchBox = document.querySelector('#search');
+let image = document.querySelectorAll('.container .wrapper');
+
+
+searchBox.oninput = () => {
+    image.forEach(hide => hide.style.display = 'none');
+    let value = search.value;
+    image.forEach(filter => {
+        let caption = filter.getAttribute('data-caption');
+    });
+};
 
 
 
 
-
-
-
-
+ 
