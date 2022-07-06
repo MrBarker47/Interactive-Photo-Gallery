8996283ea308e31f2f5417dcb831b91277f99954
MrBarker47 <jebarker7@gmail.com>
2022-07-06 12:22:07 -0400

    update files

--- js/app.js
+++ js/app.js
@@ -1,58 +1,8 @@
 baguetteBox.run('.wrapper');
 
 
-const search = new Filter('container', 'data-caption');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const itemList = [
-//     "Hay Bales",
-//     "Lake",
-//     "Canyon",
-//     "Iceberg",
-//     "Desert",
-//     "Fall",
-//     "Plantation",
-//     "Dunes",
-//     "Countryside Lane",
-//     "Sunset",
-//     "Cave",
-//     "Bluebells"
-// ];
-
-// let searchBox = document.querySelector('#search');
-// let image = document.querySelectorAll('.container .wrapper ');
-
-
-// search.oninput = () => {
-//     image.forEach(hide => hide.style.display = 'none');
-//     let value = search.value;
-//     image.forEach(filter => {
-//         let caption = filter.getAttribute('data-caption');
-//     });
-// };
 
 
 
