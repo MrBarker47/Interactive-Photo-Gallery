b514b0b94228418d80d0cebf2ef10041fd12f07c
MrBarker47 <jebarker7@gmail.com>
2022-07-11 11:54:54 -0400

    update files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 baguetteBox.run('.wrapper');
-const search = new Filter('search', 'data-caption');
+
+
+const itemList = [
+    "Hay Bales",
+    "Lake",
+    "Canyon",
+    "Iceberg",
+    "Desert",
+    "Fall",
+    "Plantation",
+    "Dunes",
+    "CountrySide Lane",
+    "Sunset",
+    "Cave",
+    "Bluebells",
+
+];
+
+
+let searchBar = document.getElementById("search").addEventListener("keyup");
 
 
 
