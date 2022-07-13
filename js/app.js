6ca833460c939e9c156a20d50eaf7dae1095d436
MrBarker47 <jebarker7@gmail.com>
2022-07-13 17:32:19 -0400

    update files

--- js/app.js
+++ js/app.js
@@ -3,6 +3,21 @@ const search = new Filter('search', 'data-caption');
 
 
 
+// let searchBox = document.querySelector('#search');
+// let image = document.querySelectorAll('.container .wrapper ');
+
+
+// search.oninput = () => {
+//     image.forEach(hide => hide.style.display = 'none');
+//     let value = search.value;
+//     image.forEach(filter => {
+//         let caption = filter.getAttribute('search','data-caption');
+//     });
+// };
+
+
+
+
 
 
 
--- js/searchFilter.js
+++ js/searchFilter.js
@@ -42,15 +42,4 @@
 
 
 
-// let searchBox = document.querySelector('#search');
-// let image = document.querySelectorAll('.container .wrapper ');
-
-
-// search.oninput = () => {
-//     image.forEach(hide => hide.style.display = 'none');
-//     let value = search.value;
-//     image.forEach(filter => {
-//         let caption = filter.getAttribute('search','data-caption');
-//     });
-// };
 
